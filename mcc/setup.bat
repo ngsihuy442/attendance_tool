6066b1005a7519825e75d62180499a802a88bf7d
ngsihuy442 <ngsihuy442@gmail.com>
2026-05-28 17:06:04 +0700

    Cập nhật quản lí nhiều máy Chấm công

--- mcc/setup.bat
+++ mcc/setup.bat
@@ -1,25 +1,9 @@
 @echo off
 title Khoi Dong Phan Mem Cham Cong
+cd /d "%~dp0"
 
-:: 1. Tự động xin quyền Administrator để đăng ký DLL hệ thống
->nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-if '%errorlevel%' NEQ '0' (
-    goto UACPrompt
-) else ( goto gotAdmin )
+:: 1. Xin quyen Admin de dang ky DLL ngam
+powershell -Command "Start-Process 'regsvr32.exe' -ArgumentList '/s', '_internal\zkemkeeper.dll' -Verb RunAs -WindowStyle Hidden"
 
-:UACPrompt
-    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
-    "%temp%\getadmin.vbs"
-    exit /B
-
-:gotAdmin
-    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
-    pushd "%CD%"
-    CD /D "%~dp0"
-
-:: 2. Đăng ký thư viện ZKTeco vào Windows (SysWOW64) ngầm
-C:\Windows\SysWOW64\regsvr32.exe /s "%~dp0_internal\zkemkeeper.dll"
-
-:: 3. Khởi chạy ứng dụng chính
-start "" "%~dp0mcc.exe"
+:: 2. Khoi chay ung dung
+start "" "mcc.exe"
